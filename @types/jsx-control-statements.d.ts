93a703fdaf402bb6f3cc410cfc685d0df06af9a6
wuwenjie <wuwenjie@xiaomai5.com>
2020-03-08 15:58:55 +0800

    feat: 添加文章的空页面

--- @types/jsx-control-statements.d.ts
+++ @types/jsx-control-statements.d.ts
@@ -1,13 +1,5 @@
-/*
- * @Author: 吴文洁
- * @Date: 2020-03-08 13:19:52
- * @LastEditors: 吴文洁
- * @LastEditTime: 2020-03-08 15:08:17
- * @Description: jsx-control-statement变量
- */
-declare const If: React.SFC<{ condition: boolean }>;
-declare const For: React.SFC<{ each: string; index: string; of: any[] }>;
-declare const Choose: React.SFC;
-declare const When: React.SFC<{ condition: boolean }>;
-declare const Otherwise: React.SFC;
-declare const With: React.SFC<{ [key: string]: any}>;
+declare var If: React.SFC<{ condition: boolean }>;
+declare var For: React.SFC<{ each: string; index?: string; of: any[] }>;
+declare var Choose: React.SFC;
+declare var When: React.SFC<{ condition: boolean }>;
+declare var Otherwise: React.SFC;
--- src/modules/article/index.tsx
+++ src/modules/article/index.tsx
@@ -2,12 +2,12 @@
  * @Author: 吴文洁
  * @Date: 2020-02-15 18:00:27
  * @LastEditors: 吴文洁
- * @LastEditTime: 2020-03-08 15:17:06
+ * @LastEditTime: 2020-03-08 15:57:23
  * @Description:
  */
 import React from 'react';
 import { observer, inject } from 'mobx-react';
-import { Input } from 'antd';
+import { Input, Empty } from 'antd';
 import SearchBar from '@/components/searchBar';
 import { BLOG_TITLE, MOTTO } from '@/domains/user-domain/constants';
 
@@ -18,6 +18,7 @@ import './index.less';
 
 interface ArticleProps {};
 interface ArticleState {};
+
 @inject('articleStore')
 @observer
 class Article extends React.Component<ArticleProps, ArticleState> {
@@ -27,23 +28,24 @@ class Article extends React.Component<ArticleProps, ArticleState> {
 
     return (
       <div className="page article-page">
-        <Choose>
-          <When condition={!!articleList.length}>
-            <div className="page-content">
-              <aside>
-                <SearchBar>
-                  <ArticleList />
-                </SearchBar>
-              </aside>
-              <section className="article">
-                <ArticleManage />
-              </section>
-            </div>
-          </When>
-          <Otherwise>
-            <div className="empty">empty</div>
-          </Otherwise>
-        </Choose>
+        {
+          !!articleList.length ?
+          <div className="page-content">
+            <aside>
+              <SearchBar>
+                <ArticleList />
+              </SearchBar>
+            </aside>
+            <section className="article">
+              <ArticleManage />
+            </section>
+          </div> :
+          <div className="empty">
+            <Empty
+              description="暂无数据"
+            />
+          </div>
+        }
       </div>
     );
   }
